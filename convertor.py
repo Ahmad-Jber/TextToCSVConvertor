80555a61b10cabd92cb91fa02f05e2367e02c382
Ahmad Jber <ahmad.jber2001@gmail.com>
2024-09-03 10:59:41 +0300

    commit

---
+++ convertor.py
@@ -0,0 +1,6 @@
+﻿import pandas as pd
+
+
+def convert_to_csv(path_txt: str, path_csv_out: str):
+    dataframe = pd.read_csv(path_txt)
+    dataframe.to_csv(path_csv_out)
---
+++ main.py
@@ -0,0 +1,5 @@
+﻿from convertor import convert_to_csv
+
+if __name__ == '__main__':
+    # convert_to_csv(path_txt="Texts/input.txt", path_csv_out="CSVs/output.csv")
+    pass
